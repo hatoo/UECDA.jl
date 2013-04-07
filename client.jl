431c819f400c885857e9ab8cbcbc809e08d949f9
hatoo <hato2000@gmail.com>
2013-04-08 00:47:52 +0900

    コメントの追加

--- client.jl
+++ client.jl
@@ -11,9 +11,11 @@ function main()
 
     whole_endflag = false
     while !whole_endflag
-        #交換前の手札と交換する枚数(富豪、大富豪のみ)
+        #交換前の手札と交換する枚数(自分が富豪大富豪の場合1,2)
         cards,num = StartGame(game)
         if num != 0
+            #とりあえず手札の中から弱い順に選ぶ
+            #カードの集合はUint64で管理する。詳細はDAbase.jl
             ex = 0u
             x=0
             for i=1:num
@@ -32,10 +34,14 @@ function main()
             if ismyturn
                 #println(tefuda)
                 try
+                #有効な手の中からランダムで提出する
                 hands = validHands(tefuda,game.info)
                 hand  = length(hands)==0?Hand():hands[rand(1:end)]
+                #ret::Bool 受理したかどうか。能動的にパスした場合も不受理判定の模様
                 ret = SendHand(game,hand)
                 if !ret&&hand!=Hand()
+                    #デバッグ用 公式のJavaのサーバーのみた特定の状況で非受理になる
+                    #Cのサーバーは大丈夫なようなのでバグかもしれない
                     println(tefuda)
                     println(hand)
                     println(game.info)
@@ -56,5 +62,3 @@ function main()
     end
     EndGame(game)
 end
-
-#0x0000000182040412
